14a4441a7b7312a9d9ba29f4b34e1b40f865d4a0
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-26 18:10:32 -0500

    Initialize project

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var config = require('config');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || config.port || 3000;
+app.listen(port);
---
+++ config_example.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+  // The port to run the app on
+  port: 4000
+
+};
