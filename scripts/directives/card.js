2ec93f9984f42ff54dcc2843f1223256a636995f
Paul Dilyard <paul.dilyard@gmail.com>
2015-04-29 00:45:58 -0400

    Add card animation in about section

---
+++ scripts/directives/card.js
@@ -0,0 +1,27 @@
+angular
+  .module('khe')
+  .directive('card', ['$compile', function ($compile) {
+
+    return {
+
+      restrict: 'E',
+      template: '<img src="/img/ace_of_hearts.svg" class="wow rotateInDownLeft animated float-left">',
+
+      link: function (scope, element, attrs) {
+        $compile(element.contents())(scope);
+
+        jQuery(document).ready(function ($) {
+
+          /**
+          * TODO !!!!
+          * Show only on screens with width > 1360px
+          */
+
+          new WOW({offset: 200}).init();
+
+        });
+      }
+
+    };
+
+  }]);
