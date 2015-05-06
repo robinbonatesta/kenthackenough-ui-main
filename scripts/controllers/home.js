252115ef63e8ff71f73674c58fb1d2f9057cd3b9
Paul Dilyard <paul.dilyard@gmail.com>
2015-05-05 23:50:13 -0400

    Refactor home controller

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -9,69 +9,122 @@ angular
       controller: 'HomeCtrl as home'
     });
   }])
-  .controller('HomeCtrl', ['User', 'News', '$location', function (User, News, $location) {
+  .controller('HomeCtrl', ['User', 'News', 'Ticket', '$location', function (User, News, Ticket, $location) {
 
-    var self = this;
-    var user = new User();
-    var news = new News();
-    self.user = user.getMe();
+    var view = this;
 
-    // Register a user
-    self.register = function () {
-      user.register({
-        email: self.email,
-        password: self.password
-      }).
-      success(function (data) {
-        self.email = '';
-        self.password = '';
-        user.setMe(data);
-        self.user = user.getMe();
-        $location.path('/application');
-      }).
-      error(function (data) {
-        if (data) {
+    var Models = {
+      user: new User(),
+      news: new News(),
+      ticket: new Ticket()
+    };
+
+    view.user = Models.user.getMe();
+
+    view.person = {
+
+      email: null,
+      password: null,
+      errors: null,
+
+      /**
+      * Register a new user
+      */
+      register: function () {
+        var self = this;
+        Models.user.register({
+          email: view.email,
+          password: view.password
+        }).
+        success(function (data) {
+          self.errors = null;
+          self.email = '';
+          self.password = '';
+          Models.user.setMe(data);
+          view.user = Models.user.getMe();
+          $location.path('/apply');
+        }).
+        error(function (data) {
+          if (data) {
+            self.errors = data.errors || ['An internal error has occurred'];
+          }
+        });
+      },
+
+      /**
+      * Login a user
+      */
+      login: function () {
+        var self = this;
+        Models.user.login({
+          email: self.email,
+          password: self.password
+        }).
+        success(function (data) {
+          self.email = null;
+          self.password = null;
+          Models.user.setMe(data);
+          view.user = Models.user.getMe();
+        }).
+        error(function (data) {
           self.errors = data.errors || ['An internal error has occurred'];
-        }
-      });
+        });
+      },
+
+      /**
+      * Logout
+      */
+      logout: function () {
+        Models.user.removeMe();
+        view.user = Models.user.getMe();
+      }
+
     };
 
-    // Login a user
-    self.login = function () {
-      user.login({
-        email: self.email,
-        password: self.password
-      }).
-      success(function (data) {
-        self.email = '';
-        self.password = '';
-        user.setMe(data);
-        self.user = user.getMe();
-      }).
-      error(function (data) {
-        self.errors = data.errors || ['An internal error has occurred'];
-      });
+    view.mail = {
+
+      email: null,
+      errors: null,
+      successes: null,
+
+      add: function () {
+        var self = this;
+        Models.news.create(self.email).
+        success(function (data) {
+          self.errors = null;
+          self.successes = ['Thanks, you\'ll be hearing from us soon!'];
+          self.email = null;
+        }).
+        error(function (data) {
+          self.errors = ['That email is already in use.'];
+          self.successes = null;
+          self.email = null;
+        });
+      }
+
     };
 
-    // Logout
-    self.logout = function () {
-      user.removeMe();
-      self.user = user.getMe();
-    };
+    view.contact = {
 
-    // Add to a mailing list
-    self.mail = function () {
-      news.create(self.email).
-      success(function (data) {
-        self.error = false;
-        self.success = true;
-        self.email = null;
-      }).
-      error(function (data) {
-        self.error = true;
-        self.success = false;
-        self.email = null;
-      });
+      new: {},
+
+      successes: null,
+      errors: null,
+
+      submit: function () {
+        var self = this;
+        Models.ticket.create(self.new).
+        success(function (data) {
+          self.errors = null;
+          self.successes = ['Thank you, one of our organizers will reach out to you soon!'];
+          self.new = {};
+        }).
+        error(function (data) {
+          self.errors = data.errors;
+          self.successes = null;
+        });
+      }
+
     };
 
   }]);
