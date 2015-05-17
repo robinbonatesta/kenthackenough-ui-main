33820521eaf9f9d3325535b00778fae61a3d0d54
Paul Dilyard <paul.dilyard@gmail.com>
2015-05-16 20:30:47 -0400

    Add angular-ui-router dependency

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ var bowerJs = [
   './bower_components/fastclick/lib/fastclick.js',
   './bower_components/angular/angular.min.js',
   './bower_components/angular-route/angular-route.min.js',
+  './bower_components/angular-ui-router/release/angular-ui-router.min.js',
   './bower_components/angular-socket-io/socket.min.js',
   './bower_components/angular-cookies/angular-cookies.min.js',
   './bower_components/moment/min/moment.min.js',
