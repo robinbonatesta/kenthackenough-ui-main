aa9d6dfa9a33107d7b967c2a83c170e6d660ae23
Paul Dilyard <paul.dilyard@gmail.com>
2015-05-18 19:25:43 -0400

    Add designed card

--- scripts/directives/card.js
+++ scripts/directives/card.js
@@ -5,17 +5,12 @@ angular
     return {
 
       restrict: 'E',
-      template: '<img src="/img/ace_of_hearts.svg" id="card" class="wow rotateInDownLeft animated float-left">',
+      template: '<img src="/img/king_of_spades.svg" id="card" class="wow rotateInDownLeft animated float-left">',
 
       link: function (scope, element, attrs) {
         $compile(element.contents())(scope);
 
         jQuery(document).ready(function ($) {
-
-          /**
-          * TODO !!!!
-          * Show only on screens with width > 1360px
-          */
 
           new WOW({offset: 200}).init();
 
