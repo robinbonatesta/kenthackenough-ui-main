a191dfd29eb85134bcb17bdb680e700534b8e67a
Paul Dilyard <paul.dilyard@gmail.com>
2015-05-27 20:57:36 -0400

    Add 404 page

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,10 @@
 angular
   .module('khe', ['ngCookies', 'btford.socket-io', 'ui.router', 'ngAnimate'])
-  .config(['$locationProvider', function ($locationProvider) {
+  .config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function ($locationProvider, $state, $route) {
     $locationProvider.html5Mode(true);
+    $route.otherwise('/404');
+    $state.state('404', {
+      url: '/404',
+      templateUrl: '/views/404.html'
+    });
   }]);
