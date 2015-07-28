d82a1df5f32dcaad45605ca6d5fad0796f57c7da
Paul Dilyard <pauldilyard@us.ibm.com>
2015-07-28 15:03:16 -0500

    Style instagram feed

--- scripts/directives/instagram.js
+++ scripts/directives/instagram.js
@@ -5,21 +5,21 @@ angular
     return {
 
       restrict: 'E',
-      templateUrl: '/views/directives/instagram.html',
+      template: '<div id="instagram"></div>',
 
       link: function (scope, element, attrs) {
         $compile(element.contents())(scope);
 
         var feed = new Instafeed({
-          clientId: '5ba607adc8384546ba7c10f6c97ac19f',
-          get: 'popular',
-          // tagName: 'car',
-          template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+          accessToken: '1523643469.467ede5.ac5cf096b0bf4745a96f0c05f842dfe8',
+          get: 'user',
+          userId: 1523643469,
+          template: '<div class="row post"><div class="small-12 columns"><div class="row"><div class="small-4 columns image"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></div><div class="small-8 columns meta"><span class="bold">Kent Hack Enough</span><br><a href="https://instagram.com/kenthackenough" target="_blank">@{{model.user.username}}</a><br><br><i class="fa fa-thumbs-up"></i> {{likes}} likes<br><i class="fa fa-comment"></i> {{comments}} comments</div></div><div class="row"><div class="small-12 columns caption">{{caption}}</div></div></div></div><hr>',
           target: 'instagram',
           sortBy: 'most-recent',
           links: true,
           limit: 3,
-          resolution: 'low_resolution'
+          resolution: 'thumbnail'
         });
         feed.run();
       }
