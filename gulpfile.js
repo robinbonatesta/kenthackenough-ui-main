20f5a7e2d4147c04ef0497012e3fcafa58570a85
Paul Dilyard <paul.dilyard@gmail.com>
2015-07-29 20:37:29 -0500

    Implement angular-moment

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ var bowerJs = [
   './bower_components/angular-socket-io/socket.min.js',
   './bower_components/angular-cookies/angular-cookies.min.js',
   './bower_components/moment/min/moment.min.js',
+  './bower_components/angular-moment/angular-moment.min.js',
   './bower_components/marked/marked.min.js',
   './bower_components/snap.svg/dist/snap.svg-min.js',
   './bower_components/wow.js/dist/wow.min.js',
--- scripts/filters/timeOfDay.js
+++
@@ -1,7 +0,0 @@
-angular
-  .module('khe')
-  .filter('timeOfDay', function () {
-    return function (input) {
-      return moment(input).format('h:mma');
-    };
-  });
